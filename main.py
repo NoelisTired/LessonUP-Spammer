24ad56bfea4944cd696e9793fb9cee6cf7f34785
NoelP <nmjj.populiers@gmail.com>
2022-11-08 13:54:51 +0100

    Supresses Continues Selenium output together with minor optimisations

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 try:
@@ -6,7 +5,7 @@ try:
     import selenium
 except:
     os.system("pip install selenium webdriver-manager")
-import random, threading
+import random, threading, logging
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -14,6 +13,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+logger = logging.getLogger('selenium.webdriver.remote.remote_connection')
+logger.setLevel(logging.ERROR)
 
 class Attributes:
     def __init__(self) -> None:
@@ -27,22 +28,15 @@ class Attributes:
     def __getnames(self):
         self.accountlist = self.__read('./names.txt')
         return [account.split('\n')[0] for account in self.accountlist]
-
+    
     @staticmethod
     def clear():
-        os.system('cls')
-        print(f"LessonUp.app autojoiner\nMade by NoelP and Sadcat\n-------------------\nUsers: {Attributes().loggedIn}")
-
+        os.system("cls" if os.name == "nt" else "clear")
+        Attributes.titlebar()
+    
+    #? Threaded function handling the join
     @staticmethod
-    def setup():
-        options = webdriver.EdgeOptions()
-        options.add_argument("--log-level=3")
-        options.add_argument('--headless')
-        driver = webdriver.Edge(
-            EdgeChromiumDriverManager().install(), options=options)
-        return driver
-    @staticmethod
-    def joinuser(driver):
+    def joinuser(driver, code, player):
         driver.get(f"https://lessonup.app/?lang=en&code={code}")
         WebDriverWait(driver, 30).until(
             EC.url_contains("https://lessonup.app/player/"))
@@ -50,22 +44,25 @@ class Attributes:
             "/html/body/div[3]/div/div[3]/div/div[1]/form/input[1]"))).send_keys(f"{player}", Keys.RETURN)
         time.sleep(1)
         driver.quit()
-        Attributes().loggedIn += 1
+        
+    #? This function initializes the driver making it usable in the thread that is activated below
     @staticmethod
     def submit(code, player):
         options = webdriver.EdgeOptions()
         options.add_argument("--log-level=3")
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_argument('--headless')
-        driver = webdriver.Edge(
-            EdgeChromiumDriverManager().install(), options=options)
-        threading.Thread(target=Attributes().joinuser, args=(driver,)).start()
-
+        driver = webdriver.Edge((EdgeChromiumDriverManager().install()), options=options)
+        threading.Thread(target=Attributes().joinuser, args=(driver,code,player)).start()
+    @staticmethod
+    def titlebar():
+        print(f"LessonUp.app autojoiner\nMade by NoelP and Sadcat\n-------------------\n")
+        time.sleep(5)
 if __name__ == "__main__":
-    Attributes.clear()
+    os.system("cls" if os.name == "nt" else "clear")
     code = input("Lessonup code: ")
+    Attributes.clear()
     for player in Attributes().names:
         Attributes.submit(code,player)
         print(f"Joined with {player}")
         time.sleep(1)
-        Attributes.clear()
-    print("End of script.")
